886228848f76942bbf656235615f96fcc3ffb355
Mike Quinn <mquinn@chemistryagency.com>
2025-12-29 12:37:37 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Main entry point for the filler-words application
+
+function greet(name) {
+  return `Hello, ${name}! Welcome to the filler-words project.`;
+}
+
+console.log(greet('World'));
+
+export default greet;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
