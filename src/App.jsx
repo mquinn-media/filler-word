75114b8347b7ccb2e9f23ede9ed6541391a36bcb
Mike Quinn <mquinn@chemistryagency.com>
2025-12-29 13:18:46 -0500

    Install Material Design library

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Container, Typography, Button, Box } from '@mui/material';
+import { Home as HomeIcon } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Filler Words</h1>
-      <p>Welcome to the Filler Words project!</p>
-    </div>
+    <Container maxWidth="md">
+      <Box sx={{ textAlign: 'center', mt: 4 }}>
+        <HomeIcon sx={{ fontSize: 60, color: 'primary.main', mb: 2 }} />
+        <Typography variant="h2" component="h1" gutterBottom>
+          Filler Words
+        </Typography>
+        <Typography variant="h6" color="text.secondary" paragraph>
+          Welcome to the Filler Words project!
+        </Typography>
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          Get Started
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
